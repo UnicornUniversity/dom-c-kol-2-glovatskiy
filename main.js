e80d3f43dfd185ce82beade5840d86cdf48bc28e
Vladislav Glovatskiy <64162767+glovatskiy@users.noreply.github.com>
2026-04-01 15:26:09 +0500

    Implement binary to decimal conversion in main function

--- main.js
+++ main.js
@@ -1,35 +1,66 @@
-//TODO add imports if needed
 //import { exMain } from "./exclude/exampleAss2.js"
-//TODO add/change doc as needed
+
 /**
- * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
- * The main function which calls the application. 
- * TODO - Please, add specific description here for the application purpose.
- * @param {string} inputNumber number that is being converted
- * @param {number} inputNumberSystem numerical system that the inputNumber is being converted from
- * @param {number} outputNumberSystem numerical system that the inputNumber is being converted into
+ * Main function which calls the application.
+ * Converts a number from one system to another.
+ * Only binary → decimal is implemented for now.
+ * @param {string} inputNumber - number that is being converted
+ * @param {number} inputNumberSystem - system number is being converted from
+ * @param {number} outputNumberSystem - system number is being converted into
  * @returns {string} containing number converted to output system
  */
+
+// Core logic is based on two loops: validation and conversion
+// Validation loop:
+// for(let i = 0; i <numString.length; i++){
+//     let digit = Number(numString[i]);
+//     if(digit !== 1 && digit !==0){
+//         return "Not a binary";
+//     }
+// }
+// Conversion loop:
+// for(let i = 0; i<numString.length; i++){
+//     let power = numString.length - 1 - i;
+//     let stringToNumber = Number(numString[i]);
+//     converted += stringToNumber*(2**power);
+// }
+
 export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
-  //TODO code
-  //let dtoOut = exMain(inputNumber, inputNumberSystem, outputNumberSystem);
-  return dtoOut;
+    // Only binary → decimal is allowed 
+    if (inputNumberSystem === 2 && outputNumberSystem === 10) {
+        const numString = inputNumber.toString();
+        let converted = 0;
+
+        for (let i = 0; i < numString.length; i++) {
+            const digit = Number(numString[i]);
+
+            if (digit !== 0 && digit !== 1) {
+                return "Not a binary";
+            }
+
+            const power = numString.length - 1 - i;
+            converted += digit * (2 ** power);
+        }
+
+        return converted.toString();
+    }
+
+    // For other systems, just return input as string
+    return inputNumber.toString();
 }
 
 /**
- * TODO - Change this to contain all input number systems that your application can convert from.
- * Function which returns which number systems are permitted on input.
- * @returns {Array} array of numbers refering to permitted input systems
+ * Returns all input number systems that your application can convert from.
+ * @returns {Array<number>}
  */
 export function permittedInputSystems() {
-	return [10, 2];
+    return [10, 2];
 }
 
 /**
- * TODO - Change this to contain all output number systems that your application can convert to.
- * Function which returns which number systems are permitted on output.
- * @returns {Array} array of numbers refering to permitted output systems
+ * Returns all output number systems that your application can convert to.
+ * @returns {Array<number>}
  */
 export function permittedOutputSystems() {
-	return [10, 2];
+    return [10, 2];
 }
